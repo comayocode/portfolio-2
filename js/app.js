fee20c8cca5919151301b0d842fbe8cc0e8f738a
roko <ronalc726@gmail.com>
2022-11-08 22:49:22 -0500

    feat: Añadir ocultar menu al hacer scroll hacia abajo

--- js/app.js
+++ js/app.js
@@ -14,3 +14,21 @@ menuLinks.forEach(menuLinks => {
         document.querySelector('.menu').classList.toggle('active');
     })
 })
+
+/* --- OCULTAR NAV AL HACER SCROLL --- */
+let ubicacionPrincipal = window.pageYOffset;
+let $nav = document.querySelector('.topnav');
+
+window.addEventListener('scroll', function() {
+    let ubicacionActual = window.pageYOffset;
+    /* --- CONDICION QUE OCULTA O MUESTRA EL NAV --- */
+    if( ubicacionPrincipal >= ubicacionActual ){
+        $nav.style.top = "0px"
+    }else {
+        $nav.style.top = "-68px"
+    }
+
+    /* --- PASAR UBICACION ACTUAL A LA UBICACION PRINCIPAL --- */
+    /* --- Permite que se esconda el nav en cualquier parte de la página*/
+    ubicacionPrincipal = ubicacionActual
+})
